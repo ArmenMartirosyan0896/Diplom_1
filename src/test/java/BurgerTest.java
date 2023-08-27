6d3998e598dd1d6fbc32ac71abcd6de6d2866bf0
Armen Martirosian <amartirosyan08@yandex.ru>
2023-08-27 17:00:52 +0400

    Diplom_1 доработанный Добавлена проверка корректности формирования чека checkCorrectReceiptTest() Так же обновлен отчет Jacoco

--- src/test/java/BurgerTest.java
+++ src/test/java/BurgerTest.java
@@ -7,6 +7,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
@@ -22,21 +23,21 @@ public class BurgerTest {
 
 
     @Test
-    public void setBunsTest(){
+    public void setBunsTest() {
         burger = Mockito.spy(new Burger());
         burger.setBuns(bun);
         Mockito.verify(burger).setBuns(bun);
     }
 
     @Test
-    public void addIngredientTest(){
+    public void addIngredientTest() {
         burger = Mockito.spy(new Burger());
         burger.addIngredient(firstIngredient);
         Mockito.verify(burger).addIngredient(firstIngredient);
     }
 
     @Test
-    public void removeIngredientTest(){
+    public void removeIngredientTest() {
         burger = Mockito.spy(new Burger());
         burger.addIngredient(firstIngredient);
         burger.removeIngredient(0);
@@ -44,16 +45,16 @@ public class BurgerTest {
     }
 
     @Test
-    public void moveIngredientTest(){
+    public void moveIngredientTest() {
         burger = Mockito.spy(new Burger());
         burger.addIngredient(firstIngredient);
         burger.addIngredient(secondIngredient);
         burger.moveIngredient(0, 1);
-        Mockito.verify(burger).moveIngredient(0,1);
+        Mockito.verify(burger).moveIngredient(0, 1);
     }
 
     @Test
-    public void getPriceTest(){
+    public void getPriceTest() {
         burger = Mockito.spy(new Burger());
         burger.setBuns(bun);
         burger.addIngredient(firstIngredient);
@@ -62,11 +63,38 @@ public class BurgerTest {
     }
 
     @Test
-    public void getReceiptTest(){
+    public void getReceiptTest() {
         burger = Mockito.spy(new Burger());
         burger.setBuns(bun);
         burger.getReceipt();
         Mockito.verify(burger).getReceipt();
+    }
+
+    @Test
+    public void checkCorrectReceiptTest() {
+
+        Mockito.when(bun.getName()).thenReturn("black bun");
+        Mockito.when(firstIngredient.getType()).thenReturn(IngredientType.SAUCE);
+        Mockito.when(firstIngredient.getName()).thenReturn("sour cream");
+        Mockito.when(secondIngredient.getType()).thenReturn(IngredientType.FILLING);
+        Mockito.when(secondIngredient.getName()).thenReturn("cutlet");
+        Mockito.when(bun.getPrice()).thenReturn(100.0f);
+        Mockito.when(firstIngredient.getPrice()).thenReturn(200.0f);
+        Mockito.when(secondIngredient.getPrice()).thenReturn(100.0f);
+
+        burger = Mockito.spy(new Burger());
+        burger.setBuns(bun);
+        burger.addIngredient(firstIngredient);
+        burger.addIngredient(secondIngredient);
+
+        String expectedReceipt = "(==== black bun ====)\n" +
+                "= sauce sour cream =\n" +
+                "= filling cutlet =\n" +
+                "(==== black bun ====)\n" +
+                String.format("%nPrice: %f%n", 500.0f);
+
+        String actualReceipt = burger.getReceipt();
+        Assert.assertEquals("Receipt format is incorrect", expectedReceipt, actualReceipt);
     }
 
 }
